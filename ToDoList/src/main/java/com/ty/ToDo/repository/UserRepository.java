1734ef261841849998f9b4d08822285776608cb9
AngelRejiK <angelitteera@gmail.com>
2025-09-30 18:32:10 +0530

    Update UserRepository.java

--- ToDoList/src/main/java/com/ty/ToDo/repository/UserRepository.java
+++ ToDoList/src/main/java/com/ty/ToDo/repository/UserRepository.java
@@ -1,10 +1,9 @@
 package com.ty.ToDo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ty.ToDo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	User findByUsername(String username);
-	boolean existsByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
