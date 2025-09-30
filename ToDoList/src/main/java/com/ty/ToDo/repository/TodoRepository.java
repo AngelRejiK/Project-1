2e4d939d66cd36bfd413c295f3673b6876d1cd11
AngelRejiK <angelitteera@gmail.com>
2025-09-30 18:31:40 +0530

    Update TodoRepository.java

--- ToDoList/src/main/java/com/ty/ToDo/repository/TodoRepository.java
+++ ToDoList/src/main/java/com/ty/ToDo/repository/TodoRepository.java
@@ -1,12 +1,13 @@
 package com.ty.ToDo.repository;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ty.ToDo.model.TodoItem;
 import com.ty.ToDo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<TodoItem, Long> {
-	 List<TodoItem> findByUser(User user);
+    List<TodoItem> findByUser(User user);
+    Optional<TodoItem> findByIdAndUser(Long id, User user);
 }
