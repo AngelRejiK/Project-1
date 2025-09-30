c4eb36dfabe89af14a2fba3c480c1afb205749d4
AngelRejiK <angelitteera@gmail.com>
2025-09-30 18:30:41 +0530

    Update User.java

--- ToDoList/src/main/java/com/ty/ToDo/model/User.java
+++ ToDoList/src/main/java/com/ty/ToDo/model/User.java
@@ -1,78 +1,33 @@
 package com.ty.ToDo.model;
-import java.util.Collection;
-import java.util.Collections;
+
+import jakarta.persistence.*;
 import java.util.List;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+@Entity
+@Table(name="users")
+public class User {
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-@Entity  // This annotation is required!
-@Table(name = "users")
-public class User implements UserDetails {
-    // your existing fields
-	@Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String username;
     private String password;
+
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<TodoItem> todos;
-    
-    
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-    	return Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
-	}
+    // Getters & Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public Long getId() {
-		return id;
-	}
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 
-	public List<TodoItem> getTodos() {
-		return todos;
-	}
-
-	public void setTodos(List<TodoItem> todos) {
-		this.todos = todos;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	@Override
-    public String getPassword() { return getPassword(); }
-
-    @Override
-    public String getUsername() { return getUsername(); }
-
-    @Override
-    public boolean isAccountNonExpired() { return true; }
-
-    @Override
-    public boolean isAccountNonLocked() { return true; }
-
-    @Override
-    public boolean isCredentialsNonExpired() { return true; }
-
-    @Override
-    public boolean isEnabled() { return true; }
+    public List<TodoItem> getTodos() { return todos; }
+    public void setTodos(List<TodoItem> todos) { this.todos = todos; }
 }
