e5cdff29afcad29a525107b30e46a765b5097118
AngelRejiK <angelitteera@gmail.com>
2025-09-30 18:34:33 +0530

    Update UserService.java

--- ToDoList/src/main/java/com/ty/ToDo/service/UserService.java
+++ ToDoList/src/main/java/com/ty/ToDo/service/UserService.java
@@ -1,47 +1,28 @@
 package com.ty.ToDo.service;
 
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import com.ty.ToDo.model.User;
+import com.ty.ToDo.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.ty.ToDo.model.User;
-import com.ty.ToDo.repository.UserRepository;
+import java.util.Optional;
 
-import jakarta.validation.Valid;
 @Service
-public class UserService implements UserDetailsService {
-	
-	private final UserRepository userRepository;
-	private final PasswordEncoder passwordEncoder;
-	
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-	    return userRepository.findByUsername(username);
-	}
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
-	public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-		super();
-		this.userRepository = userRepository;
-		this.passwordEncoder = passwordEncoder;
-	}
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
 
-
-	public void save(@Valid User user) {
-		user.setPassword(passwordEncoder.encode(user.getPassword()));
+    public void save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
-		
-	}
-	
-	public User findByUsername(String username) {
-	    return userRepository.findByUsername(username);
-	}
-
-
-	public boolean existsByUsername(String username) {
-		return userRepository.existsByUsername(username);
-	}
-
+    }
 }
